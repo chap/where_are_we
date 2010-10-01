03c3ec1b9627206f0b70740722893b7676368ff7
Chap Ambrose <chapambrose@gmail.com>
2010-10-01 10:09:41 -0500

    first commit

---
+++ where_are_we/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhereAreWe::Application.config.secret_token = '73924934fad18c2b233bbf8de914ce6c3209ac54f207931a8ef14b039a58b8817adf4b18c31215dad7a7344aa7497db5b682c9a7d1321fcc36ca961a13fe0546'
