4b15f0e4b850762fde4a3ade698549189fe5b932
Chap Ambrose <chapambrose@gmail.com>
2010-10-01 10:33:00 -0500

    got basic lat and long finding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 WhereAreWe::Application.routes.draw do
+  root :to => "visits#new"
+  resources :visits
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
